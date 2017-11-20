332ff49c9ff6f77a87539e4a7ce12b604004e3e1
Sankar Rengarajan <mulshankar@gmail.com>
2017-11-20 16:05:11 -0600

    code

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -99,7 +99,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 		  0, 0, 1, 0,
 		  0, 0, 0, 1;
 	
-	float previous_timestamp=measurement_pack.timestamp_;
+	previous_timestamp=measurement_pack.timestamp_;
 	
 	ekf_.Q_ << 1, 0, 1, 0,
 		  0, 1, 0, 1,
